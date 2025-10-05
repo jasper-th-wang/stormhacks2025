e63d8648238567ab2972b83b280e69c51f7e00e1
Jasper Wang <jasper8777@icloud.com>
2025-10-04 20:21:28 -0700

    wire up to popup

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
 // When popup opens, get the stored text and display it
-chrome.storage.local.get(['selectedText'], (result) => {
-  if (result.selectedText) {
-    document.getElementById('selectedText').textContent = result.selectedText;
+chrome.storage.local.get(['word'], (result) => {
+  if (result.word) {
+      document.getElementById('word').textContent = "word: " + result.word + "\n";
+  }
+});
+chrome.storage.local.get(['paragraph'], (result) => {
+  if (result.paragraph) {
+      document.getElementById('paragraph').textContent = "paragraph: " + result.paragraph + "\n";
+  }
+});
+chrome.storage.local.get(['sentence'], (result) => {
+  if (result.sentence) {
+      document.getElementById('sentence').textContent = "sentence: " + result.sentence;
   }
 });
