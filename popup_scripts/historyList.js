5f6037f533cf4dd05078ab7a7a45c95471aca285
Matt Wiens <mwiens91@gmail.com>
2025-10-05 11:09:51 -0700

    Fixup handling of popup UI when no history

--- popup_scripts/historyList.js
+++ popup_scripts/historyList.js
@@ -1,20 +1,16 @@
 chrome.storage.local.get({ history: [] }, async (result) => {
   const entryList = result.history.reverse();
+  const listDiv = document.getElementById("list");
 
   if (entryList.length === 0) {
+    listDiv.textContent = "No history";
     return;
   }
-
-  // Show the title
-  document.getElementById("listTitle").style.display = "block";
 
   // Get the active tab
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   // List the search history
-  // TODO: make this look not terrible
-  const listDiv = document.getElementById("list");
-
   for (const entry of entryList) {
     // Make div and have it listen to click events
     const div = document.createElement("div");
