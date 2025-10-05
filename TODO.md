68a5645e92d36b8deeeb50b29e3157fef8936e2d
Matt Wiens <mwiens91@gmail.com>
2025-10-05 09:42:57 -0700

    Add on-click interaction for history items to bring up modal

--- TODO.md
+++ TODO.md
@@ -1,6 +1,4 @@
 # TODO
 
 + have processed results shown in a modal (not an alert)
-+ make extension popup look not terrible
-+ make clicking on extension popup history items bring up modal
 + add option to delete popup history items
--- popup_scripts/historyList.js
+++ popup_scripts/historyList.js
@@ -1,5 +1,5 @@
-chrome.storage.local.get({ history: [] }, (result) => {
-  const entryList = result.history;
+chrome.storage.local.get({ history: [] }, async (result) => {
+  const entryList = result.history.reverse();
 
   if (entryList.length === 0) {
     return;
@@ -8,12 +8,21 @@ chrome.storage.local.get({ history: [] }, (result) => {
   // Show the title
   document.getElementById("listTitle").style.display = "block";
 
+  // Get the active tab
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
   // List the search history
   // TODO: make this look not terrible
   const listDiv = document.getElementById("list");
 
   for (const entry of entryList) {
+    // Make div and have it listen to click events
     const div = document.createElement("div");
+    div.className = "action";
+    div.addEventListener("click", () => {
+      chrome.tabs.sendMessage(tab.id, {action: "showModal", entryId: entry.id})
+    });
+
     const date = new Date(entry.date).toLocaleString("en-CA", {
       dateStyle: "medium",
       timeStyle: "short",
