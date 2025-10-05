ae1614cba3961b8e6fc1c2d61f1d74596782a5db
Jasper Wang <jasper8777@icloud.com>
2025-10-05 09:54:32 -0700

    remove debuging code

--- popup_scripts/popup.js
+++ popup_scripts/popup.js
@@ -1,16 +1,17 @@
+// FOR DEBUGING
 // When popup opens, get the stored text and display it
-chrome.storage.local.get(['word'], (result) => {
-  if (result.word) {
-      document.getElementById('word').textContent = "word: " + result.word + "\n";
-  }
-});
-chrome.storage.local.get(['paragraph'], (result) => {
-  if (result.paragraph) {
-      document.getElementById('paragraph').textContent = "paragraph: " + result.paragraph + "\n";
-  }
-});
-chrome.storage.local.get(['sentence'], (result) => {
-  if (result.sentence) {
-      document.getElementById('sentence').textContent = "sentence: " + result.sentence;
-  }
-});
+// chrome.storage.local.get(['word'], (result) => {
+//   if (result.word) {
+//       document.getElementById('word').textContent = "word: " + result.word + "\n";
+//   }
+// });
+// chrome.storage.local.get(['paragraph'], (result) => {
+//   if (result.paragraph) {
+//       document.getElementById('paragraph').textContent = "paragraph: " + result.paragraph + "\n";
+//   }
+// });
+// chrome.storage.local.get(['sentence'], (result) => {
+//   if (result.sentence) {
+//       document.getElementById('sentence').textContent = "sentence: " + result.sentence;
+//   }
+// });
